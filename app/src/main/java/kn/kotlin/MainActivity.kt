390c35c6ba06742bbbac6a743377be56a839d373
xkn <>
2018-10-22 18:02:01 +0800

    no message

---
+++ app/src/main/java/kn/kotlin/MainActivity.kt
@@ -0,0 +1,19 @@
+package kn.kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+
+    }
+
+    private fun initView() {
+
+    }
+
+
+}
